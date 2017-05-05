6cfd8c9fc693a102354cb87c885ab753388100d9
yakivvv <yakivvv@gmail.com>
2017-05-05 11:10:06 +0400

    Create mssql_install.ps1

---
+++ mssql_install.ps1
@@ -0,0 +1,4 @@
+$beforeMount = (Get-Volume).DriveLetter
+$mountResult = Mount-DiskImage C:\tmp\SQLServer2014SP2.iso
+$setuppath = (compare $beforeMount (Get-Volume).DriveLetter -PassThru) + «:\setup.exe»
+& $setuppath /ConfigurationFile=C:\ansible\ConfigurationFile.INI /Q
